20ab8337c3cd3378ba90949376ab9ebb49b3fe47
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-11-25 12:44:00 +0100

    Red

--- src/model/Fermentable.spec.ts
+++ src/model/Fermentable.spec.ts
@@ -1,14 +1,23 @@
+import Validate from '../validation/Validate'
 import Fermentable from './Fermentable'
 import { expect } from 'chai'
 import { suite, test } from 'mocha-typescript'
+import { stub } from 'sinon'
 
 @suite
 class FermentableTest {
   private sut: Fermentable
   private yield: number = 0.75
+  private validatePercentStub: Sinon.SinonStub
+  private validateNotNegativeStub: Sinon.SinonStub
 
   public before () {
     this.sut = new Fermentable(this.yield)
+    this.validatePercentStub = stub(Validate, 'percent')
+  }
+
+  public after () {
+    this.validatePercentStub.restore()
   }
 
   @test
@@ -23,5 +32,11 @@ class FermentableTest {
     this.sut.yield = newYield
 
     expect(this.sut.yield).to.equal(newYield)
+  }
+
+  @test
+  public shouldValidateYieldAsPercent () : void {
+    this.sut.yield = 0.60
+    expect(this.validatePercentStub.calledOnce).to.be.true
   }
 }
