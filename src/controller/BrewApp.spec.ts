3114b4734429f0ceab7f9d06228489306c456faa
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-11-27 22:14:38 +0100

    Refactor

--- src/controller/BrewApp.spec.ts
+++ src/controller/BrewApp.spec.ts
@@ -4,12 +4,12 @@ import { expect } from 'chai'
 import { createStubInstance } from 'sinon'
 
 describe('Class BrewApp', () => {
+  let viewMock: ViewMock = <ViewMock> createStubInstance(View)
+  let sut: BrewApp = new BrewApp(<any> viewMock)
+
   describe('Method', () => {
     describe('init', () => {
       it('Should tell view to render', () => {
-        let viewMock: ViewMock = <ViewMock> createStubInstance(View)
-        let sut: BrewApp = new BrewApp(<any> viewMock)
-
         sut.init()
 
         expect(viewMock.render.calledOnce).to.be.true
