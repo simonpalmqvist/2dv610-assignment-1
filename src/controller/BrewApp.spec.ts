894ff3da7429337c2f9ea587f431f6d0e36a8899
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-11-28 20:35:42 +0100

    Refactor

--- src/controller/BrewApp.spec.ts
+++ src/controller/BrewApp.spec.ts
@@ -5,22 +5,23 @@ import { expect } from 'chai'
 import { createStubInstance } from 'sinon'
 
 describe('Class BrewApp', () => {
+  let viewMock: ViewMock
+  let sut: BrewApp
+
+  beforeEach(() => {
+    viewMock = <ViewMock> createStubInstance(View)
+    sut = new BrewApp(<any> viewMock)
+  })
 
   describe('Method', () => {
     describe('init', () => {
       it('Should tell view to render', () => {
-        let viewMock: ViewMock = <ViewMock> createStubInstance(View)
-        let sut: BrewApp = new BrewApp(<any> viewMock)
-
         sut.init()
 
         expect(viewMock.render.calledOnce).to.be.true
       })
 
       it('Should add listener for ADD_HOPS action', () => {
-        let viewMock: ViewMock = <ViewMock> createStubInstance(View)
-        let sut: BrewApp = new BrewApp(<any> viewMock)
-
         sut.init()
 
         expect(viewMock.on.calledWith(Action.ADD_HOP)).to.be.true
