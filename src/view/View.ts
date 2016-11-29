0c26553478e1c965ad8bd41343582d66eb59a9a6
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-11-29 18:14:14 +0100

    Green

--- src/view/View.ts
+++ src/view/View.ts
@@ -3,12 +3,14 @@ import { State } from './State'
 import { EventEmitter } from 'events'
 
 export class View extends EventEmitter {
+  private _ui: ConsoleUI
 
   constructor (ui: ConsoleUI) {
     super()
+    this._ui = ui
   }
-  
+
   public render (state: State) : void {
-    console.warn('implement me')
+    this._ui.print('Welcome to this beer brewing app!')
   }
 }
