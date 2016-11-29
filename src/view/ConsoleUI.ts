83add4a71e2eba548ee58d6a64d59f93958ee3c0
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-11-29 16:51:31 +0100

    Green

--- src/view/ConsoleUI.ts
+++ src/view/ConsoleUI.ts
@@ -2,13 +2,15 @@ import * as readline from 'readline'
 
 export class ConsoleUI {
   private _output: Console
+  private _input: readline.ReadLine
 
   constructor(output: Console, rl: readline.ReadLine) {
     this._output = output
+    this._input = rl
   }
 
   public registerInputHandler(callback: () => void) {
-
+    this._input.on('line', callback)
   }
 
   public print (message: string) : void {
