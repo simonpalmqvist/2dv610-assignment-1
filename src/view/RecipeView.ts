c8f39054667168bb1a4a713038a797c18bc2a83d
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-12-02 15:38:53 +0100

    Green

--- src/view/RecipeView.ts
+++ src/view/RecipeView.ts
@@ -64,12 +64,12 @@ export class RecipeView {
   public async showAddFermentableForm () : Promise<FermentableForm> {
     const name: string = await this._ui.askQuestion('Name of fermentable: ')
     const amount: string = await this._ui.askQuestion('Amount (kg): ', this._validateNotNegative)
-    await this._ui.askQuestion('Yield (%) [0-100]: ', this._validatePercent)
+    const yieldPercent: string = await this._ui.askQuestion('Yield (%) [0-100]: ', this._validatePercent)
 
     return {
       amount: +amount,
       name,
-      yieldPercent: 0,
+      yieldPercent: +yieldPercent / 100,
     }
   }
 
